6d3e23f720a05a782d9d33e0bbde52fe4e149155
sungjuune <ksjsungjune@korea.ac.kr>
2024-08-28 21:59:36 +0900

    update

--- gpt.py
+++ gpt.py
@@ -19,16 +19,17 @@ def GPT(instruction):
 
 def main():
     global CLIENT, SYSTEM_CONTENT
-    CLIENT = OpenAI(api_key = "///")
+    CLIENT = OpenAI(api_key = "sk-ket5TWTBO2lu5W7EhJFlHxf_irgCakAPO_kV3ChS3ST3BlbkFJJ1X48T5jZbNfAg3lRxDNt3H6rQ-p2TPHn0TdhrbCkA")
     SYSTEM_CONTENT = \
 """
 From now on, you will be my helpful assistant on paraphrasing the\
 language instruction for a navigation robot.\
 Preserving the context and the goal of the given instruction,\
 paraphrase the sentences using new vocabularies, new phrases, and new tone of voice, \
-as if the instructions are given by another human user. 
+as if the instructions are given by another human user. \
+Please do not generate any reply for this content prompt, and just generate the paraphrased instructions.
 """
-    with open("REVERIE_instruction.json", 'r') as file:
+    with open("./REVERIE_val_seen_original.json", 'r') as file:
         instructions = json.load(file)
 
     dict_ = {}
@@ -37,9 +38,8 @@ as if the instructions are given by another human user.
         result = GPT(instruction_list).split('\n')
         dict_[key] = result
 
-    with open("new_instruction.json", "w") as json_file:
+    with open("./REVERIE_val_seen_new.json", "w") as json_file:
         json.dump(dict_, json_file)        
-
 
 
 if __name__ == "__main__":
